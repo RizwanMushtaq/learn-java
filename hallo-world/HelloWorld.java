ce2db3c0c40795cb4ea414687477c9c5b318bfda
Rizwan <rizwanmushtaq15@gmail.com>
2025-06-15 18:12:26 +0200

    initial commit

---
+++ hallo-world/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String message = "Hallo World";
+        System.out.println(message);
+        System.out.println("It's me, Rizwan here");
+    }
+}

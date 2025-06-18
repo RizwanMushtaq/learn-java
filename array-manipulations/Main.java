70f3fa830a470f74b085d30512980d8b85a345e1
Rizwan <rizwanmushtaq15@gmail.com>
2025-06-18 10:28:34 +0200

    added array-manipulations

---
+++ array-manipulations/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+  public static void main(String[] args) {
+    String[] copyFrom = { "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado", "Doppio", "Espresso",
+        "Frappucino", "Freddo", "Lungo", "Macchiato", "Marocchino", "Ristretto" };
+    String[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+    // Print the original array
+    printArray(copyFrom);
+    // Print the copied array
+    printArray(copyTo);
+    // Check if the two arrays are equal
+    System.out.println(java.util.Arrays.equals(copyFrom, copyTo));
+  }
+
+  // Generic print method for any type of array
+  public static <T> void printArray(T[] array) {
+    System.out.println("Array size: " + array.length);
+    for (T item : array) {
+      System.out.print(item + " ");
+    }
+    System.out.println('\n');
+  }
+}

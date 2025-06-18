ada80d5dc4d3f5c920350a7f5b99f876e92bc2fe
Rizwan <rizwanmushtaq15@gmail.com>
2025-06-18 11:32:53 +0200

    updated Bicycles

--- Bicycles/Main.java
+++ Bicycles/Main.java
@@ -6,33 +6,39 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Creating a MountainBike object...");
     MountainBike myBike = new MountainBike(10, 2, 30, 1);
-    System.out.println("Seat Height: " + myBike.seatHeight);
+    System.out.println("Seat Height: " + myBike.getHeight());
   }
 }
 
 class Bicycle {
-  // the Bicycle class has
-  // three fields
-  public int cadence;
-  public int gear;
-  public int speed;
+  private int cadence;
+  private int gear;
+  private int speed;
 
-  // the Bicycle class has
-  // one constructor
   public Bicycle(int startCadence, int startSpeed, int startGear) {
     gear = startGear;
     cadence = startCadence;
     speed = startSpeed;
   }
 
-  // the Bicycle class has
-  // four methods
+  public int getCadence() {
+    return cadence;
+  }
+
   public void setCadence(int newValue) {
     cadence = newValue;
   }
 
   public void setGear(int newValue) {
     gear = newValue;
+  }
+
+  public int getGear() {
+    return gear;
+  }
+
+  public int getSpeed() {
+    return speed;
   }
 
   public void applyBrake(int decrement) {
@@ -45,20 +51,18 @@ class Bicycle {
 }
 
 class MountainBike extends Bicycle {
-  // the MountainBike subclass has
-  // one field
-  public int seatHeight;
+  private int seatHeight;
 
-  // the MountainBike subclass has
-  // one constructor
   public MountainBike(int startHeight, int startCadence, int startSpeed, int startGear) {
     super(startCadence, startSpeed, startGear);
     seatHeight = startHeight;
   }
 
-  // the MountainBike subclass has
-  // one method
   public void setHeight(int newValue) {
     seatHeight = newValue;
+  }
+
+  public int getHeight() {
+    return seatHeight;
   }
 }

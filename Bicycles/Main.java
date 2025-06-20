03b5ac726ec137163f47b0775b55878b23f1bb9c
Rizwan <rizwanmushtaq15@gmail.com>
2025-06-20 21:24:12 +0200

    updated Bicylcle Project

--- Bicycles/Main.java
+++ Bicycles/Main.java
@@ -4,10 +4,13 @@ package Bicycles;
 // by creating a Bicycle class and a MountainBike subclass.
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Creating a MountainBike object...");
-    MountainBike myBike = new MountainBike(10, 2, 30,
-        "high");
-    myBike.printDescription();
+    Bicycle bike01, bike02, bike03;
+    bike01 = new Bicycle(20, 10, 1);
+    bike02 = new MountainBike(20, 10, 5, "Dual");
+    bike03 = new RoadBike(40, 20, 8, 23);
+    bike01.printDescription();
+    bike02.printDescription();
+    bike03.printDescription();
   }
 }
 
@@ -92,5 +95,31 @@ class MountainBike extends Bicycle {
     super.printDescription();
     System.out.println("The " + "MountainBike has a "
         + getSuspension() + " suspension.");
+  }
+}
+
+class RoadBike extends Bicycle {
+  // In millimeters (mm)
+  private int tireWidth;
+
+  public RoadBike(int startCadence, int startSpeed,
+      int startGear, int newTireWidth) {
+    super(startCadence, startSpeed, startGear);
+    this.tireWidth = newTireWidth;
+  }
+
+  public int getTireWidth() {
+    return this.tireWidth;
+  }
+
+  public void setTireWidth(int newTireWidth) {
+    this.tireWidth = newTireWidth;
+  }
+
+  @Override
+  public void printDescription() {
+    super.printDescription();
+    System.out.println("The RoadBike" + " has "
+        + getTireWidth() + " MM tires.");
   }
 }

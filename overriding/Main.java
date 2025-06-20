6bd05dec7660db01982a454866a6324ff1d29a7d
Rizwan <rizwanmushtaq15@gmail.com>
2025-06-20 20:47:02 +0200

    added Overrides concept

---
+++ overriding/Main.java
@@ -0,0 +1,31 @@
+class Animal {
+  public static void testClassMethod() {
+    System.out.println("The static method in Animal");
+  }
+
+  public void testInstanceMethod() {
+    System.out.println("The instance method in Animal");
+  }
+}
+
+class Cat extends Animal {
+  public static void testClassMethod() {
+    System.out.println("The static method in Cat");
+  }
+
+  @Override
+  public void testInstanceMethod() {
+    System.out.println("The instance method in Cat");
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Cat myCat = new Cat();
+    Animal myAnimal = myCat;
+    Animal.testClassMethod();
+    Cat.testClassMethod();
+    myCat.testInstanceMethod();
+    myAnimal.testInstanceMethod();
+  }
+}

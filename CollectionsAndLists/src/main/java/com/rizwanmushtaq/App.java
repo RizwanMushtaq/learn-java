39487dd5fb2164ae8d60426f087f48fb70120463
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-03 21:09:23 +0200

    added List concepts

--- CollectionsAndLists/src/main/java/com/rizwanmushtaq/App.java
+++ CollectionsAndLists/src/main/java/com/rizwanmushtaq/App.java
@@ -1,13 +1,17 @@
 package com.rizwanmushtaq;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class App {
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    list.add("Cherry");
+    list.add("Apple");
+    list.add("Banana");
+    System.out.println("List contents: " + list);
+    Collections.sort(list);
+    System.out.println("Sorted list: " + list);
+  }
 }

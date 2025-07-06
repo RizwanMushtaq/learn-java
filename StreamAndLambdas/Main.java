f9990d87fcc613b73b54d03c35e53917b21f09e6
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-06 12:22:12 +0200

    added stream operations

--- StreamAndLambdas/Main.java
+++ StreamAndLambdas/Main.java
@@ -1,12 +1,39 @@
-public void Main() {
-  public static void main(String[] args){
-    // Example usage of the Stream and Lambda expressions
-    List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David");
-    // Using a stream to filter names that start with 'A'
-    List<String> filteredNames = names.stream()
-        .filter(name -> name.startsWith("A"))
-        .collect(Collectors.toList());
-    // Printing the filtered names
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    List<String> names = Arrays.asList("Zaid","John","Alice", "Bob", "Charlie",
+        "David");
+    printNamesWithA(names);
+    printNamesWithLimit2(names);
+    printNamesWithSorted(names);
+  }
+
+  public static void printNamesWithA(List<String> names) {
+    System.out.println("--- Filtered Names ---");
+    List<String> filteredNames =
+        names.stream()
+            .filter(name -> name.startsWith("A"))
+            .collect(Collectors.toList());
     filteredNames.forEach(System.out::println);
+  }
+
+  public static void printNamesWithLimit2(List<String> names) {
+    System.out.println("--- Limited Names ---");
+    List<String> limitedNames =
+        names.stream()
+            .limit(2)
+            .collect(Collectors.toList());
+    limitedNames.forEach(System.out::println);
+  }
+
+  public static void printNamesWithSorted(List<String> names) {
+    System.out.println("--- Sorted Names ---");
+    List<String> sortedNames =
+        names.stream()
+            .sorted()
+            .collect(Collectors.toList());
+    sortedNames.forEach(System.out::println);
   }
 }

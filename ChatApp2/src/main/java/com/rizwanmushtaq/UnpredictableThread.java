631554823f4af54545e62cb8e90eace994002e30
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-15 19:24:03 +0200

    added UnpredictableThread.java

---
+++ ChatApp2/src/main/java/com/rizwanmushtaq/UnpredictableThread.java
@@ -0,0 +1,13 @@
+package com.rizwanmushtaq;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class UnpredictableThread {
+  public static void main(String[] args) {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    executorService.execute(() -> System.out.println("Hello from a thread!"));
+    System.out.println("Hello from the main thread!");
+    executorService.shutdown();
+  }
+}
